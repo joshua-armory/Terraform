abe60f6c4703a1addf7684d09f582971f323d4e5
joshua-armory <99360899+joshua-armory@users.noreply.github.com>
2022-05-16 09:16:27 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 terraform {
     backend "remote" {
         hostname = "app.terraform.io"
-        organization = "Remote"
+        organization = "Arm11"
         token = "pJdqHvJzVJIevw.atlasv1.uye3qcfL2zz1pVg4TQ72ZKvg0LhBRGVRnqkfO9ce06YrGs9Ru8L8j2psarSkuchcpWs"
 
         workspaces {
-            name = "env-prod"
+            name = "Remote"
         }
     }
 }
