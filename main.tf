1c986194cb7aed8f9f33816595cba31ec4afa8f5
joshua-armory <99360899+joshua-armory@users.noreply.github.com>
2022-08-19 19:21:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "Arm11"
+
+        workspaces {
+            name = "Remote-test"
+        }
+    }
+}
+
+
 output "current_workspace_name" {
   value = terraform.workspace
 }
